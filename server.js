3e83cec614f51c0c3d57b1608dc70cd3862c9ef0
Servio Mora <servio.mora19@gmail.com>
2018-01-23 20:06:40 -0500

    react and webpack configured

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const db = require('./config/db');
 
 app.use(logger('dev'));
 
+app.use('/', express.static(__dirname + '/public'));
+
 //Server Setup
 const PORT = process.env.PORT || 3000;
 const server = http.createServer(app);
